4201d39710cdc9904914cec16a1382732c2b919f
Oscar van Tol <o.van.tol@betabit.nl>
2021-06-15 21:37:17 +0200

    Added IOptions example.

--- ExampleFunction/HttpFunction.cs
+++ ExampleFunction/HttpFunction.cs
@@ -1,20 +1,27 @@
-using System.Net;
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace ExampleFunction
 {
     public class HttpFunction
     {
+        readonly ExampleConfig _exampleConfig;
+
+        public HttpFunction(IOptions<ExampleConfig> options)
+        {
+            _exampleConfig = options.Value;
+        }
+
         [Function(nameof(HttpFunction1))]
         public async Task<string> HttpFunction1([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "test")] HttpRequestData req, FunctionContext functionContext)
         {
             var log = functionContext.GetLogger<HttpFunction>();
             log.LogInformation("You called the trigger!");
 
-            return "Hello world";
+            return _exampleConfig.Greeting;
         }
 
         [Function(nameof(HttpFunction2))]
--- ExampleFunction/Program.cs
+++ ExampleFunction/Program.cs
@@ -1,6 +1,7 @@
 using ExampleFunction;
 using Microsoft.Azure.Functions.Worker.Configuration;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 
@@ -16,7 +17,7 @@ var host = new HostBuilder()
     })
     .ConfigureServices(s =>
     {
-
+        s.AddOptions<ExampleConfig>().Configure<IConfiguration>((s, c) => c.GetSection("ExampleConfig").Bind(s));
     })
     .Build();
 
